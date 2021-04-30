f4a633f9b88c65bcac3eac00c5ae119f109cf7ff
zhang'kai <2993258983@qq.com>
2021-04-30 17:18:01 +0800

    1

--- app/src/main/java/com/example/ouchaixun/Activity/NewsDetilsActivity.java
+++ app/src/main/java/com/example/ouchaixun/Activity/NewsDetilsActivity.java
@@ -3,6 +3,8 @@ package com.example.ouchaixun.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
 import com.example.ouchaixun.R;
 
 public class NewsDetilsActivity extends AppCompatActivity {
@@ -11,5 +13,17 @@ public class NewsDetilsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_news_detils);
+
+
+
+
+
+
+        findViewById(R.id.news_detils_back).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
